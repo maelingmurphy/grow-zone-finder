122e92ae20c4656a4a1f104e6585660396f79ff9
Maeling <maelingmurphy@users.noreply.github.com>
2021-06-15 09:05:19 -0400

    create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return(
+        <div className="header">
+            <h1 className="header-title">Plant Hardiness Zone Finder</h1>
+        </div>
+        
+    );
+}
+
+export default Header;
