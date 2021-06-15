4ae559636f5de443cf0e206ed0df89e01f8cae18
Maeling <maelingmurphy@users.noreply.github.com>
2021-06-15 09:35:18 -0400

    import Form component into App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header'
+import Form from './components/Form'
 
 const App = () => {
     return (
       <div className="App">
         <Header />
+        <Form />
       </div>
     );
   }
