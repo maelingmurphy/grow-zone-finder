93a2bd39725b5f63e54fb0f277d0de7b45ee31ab
Maeling <maelingmurphy@users.noreply.github.com>
2021-06-15 09:46:52 -0400

    import and set PropTypes for Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,15 @@
+import PropTypes from 'prop-types'
+
 const Button = ({ text }) => {
     return (
         <div>
             <button className='btn'>{text}</button>
         </div>
     );
+}
+
+Button.propTypes = {
+    text: PropTypes.string
 }
 
 export default Button;
