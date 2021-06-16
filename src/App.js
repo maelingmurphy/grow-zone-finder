e03e0ce6913fbba91251116aafca1ffd29781076
Maeling <maelingmurphy@users.noreply.github.com>
2021-06-16 19:30:43 -0400

    Lift state up so Results component will have access to all state variables and related functions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,76 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Form from './components/Form'
 import Results from './components/Results'
 
+
 const App = () => {
-    return (
-      <div className="App">
-        <Header />
-        <Form />
-        <Results />
-      </div>
-    );
+ 
+  // Declare a new state variable, zipCode, that is linked to input value
+  const [zipCode, setzipCode] = useState('');
+
+  // set state variable for zone data so it can be displayed in results
+  // const [zone, setZone] = useState(5);
+  
+  // function for updating zipCode state as input changes
+  const onChange = (e) => {
+    setzipCode(e.target.value);
+  } 
+  
+  // function for checking zip code format
+  const isCorrectZipCodeFormat = (zipcode) => {
+    const regexpFiveDigit = /^[0-9]{5}$/;
+    return regexpFiveDigit.test(zipcode) ;
   }
+
+  // function for getting zone data
+  const getZoneData = async (zipcode) => {
+    try {
+        // make 'GET' request to API phzmapi.org
+        const response = await fetch(`https://phzmapi.org/${zipcode}.json`);
+        
+        // check if response is successful (ok)
+        if (!response.ok) {
+            // throw error if response not ok
+            throw new Error(response.status);
+        }
+        // convert Response object to JSON 
+        const zoneData = await response.json();
+        
+        // log zoneData
+        console.log(zoneData)
+
+    } catch (error) {
+        console.log(error)
+        alert(`${error} : No data found for this zip code entry (${zipCode})`);
+    }
+  }
+
+  // function for retrieving & displaying zone data via button click 
+  const onClick = (event) => {
+    event.preventDefault(); // prevents submission to a page
+    console.log(`Clicked`, zipCode);
+    console.log('result', isCorrectZipCodeFormat(zipCode));
+    // validate zip code format
+    if (!(isCorrectZipCodeFormat(zipCode))) {
+        alert('Please enter a 5 digit zip code');
+        return;
+    }
+
+    // get zoneData
+    getZoneData(zipCode);
+
+  }
+
+  return (
+    <div className="App">
+      {/* <button onClick={addZone}>Test zone data: { zoneData }</button>  */}
+      <Header />
+      <Form onClick={onClick} zipCode={zipCode} onChange={onChange}/>
+      <Results />
+    </div>
+  );
+}
   
   export default App;
   
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,54 +1,7 @@
-import { useState } from 'react'
 import Button from './Button'
 
-const Form = () => {
-    // Declare a new state variable, zipCode, that is linked to input value
-    const [zipCode, setZipCode] = useState('');
-    
-    // function for checking zip code format
-    const isCorrectZipCodeFormat = (zipcode) => {
-        const regexpFiveDigit = /^[0-9]{5}$/;
-        return regexpFiveDigit.test(zipcode) ;
-    }
-
-    // function for getting zone data
-    const getZoneData = async (zipcode) => {
-        try {
-            // make 'GET' request to API phzmapi.org
-            const response = await fetch(`https://phzmapi.org/${zipcode}.json`);
-            
-            // check if response is successful (ok)
-            if (!response.ok) {
-                // throw error if response not ok
-                throw new Error(response.status);
-            }
-            // convert Response object to JSON 
-            const zoneData = await response.json();
-            
-            // log zoneData
-            console.log(zoneData.zone);
-            return zoneData.zone;
-        } catch (error) {
-            console.log(error)
-            alert(`${error} : No data found for this zip code entry (${zipCode})`);
-        }
-    }
-    
-    // function for retrieving & displaying zone data
-    const onClick = (event) => {
-        event.preventDefault(); // prevents submission to a page
-        console.log(`Clicked`, {zipCode});
-        console.log('result', isCorrectZipCodeFormat(zipCode));
-        // validate zip code format
-        if (!(isCorrectZipCodeFormat(zipCode))) {
-            alert('Please enter a 5 digit zip code');
-            return;
-        }
-
-        // get zoneData
-        getZoneData(zipCode);
-    }
-
+const Form = ({ onClick, zipCode, onChange }) => {
+  
     return (
         <div className='form-container'>
             <form className='form'>
@@ -59,7 +12,7 @@ const Form = () => {
                         placeholder='Enter zipcode'
                         name='zipcode'
                         value={zipCode}
-                        onChange={(e) => setZipCode(e.target.value)}
+                        onChange={onChange}
                     />
                     <Button text='Find My Zone' onClick={onClick}/>
                 </div>
