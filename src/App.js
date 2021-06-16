16024bbcfd3b65b6356afc39e7b3f9252485e936
Maeling <maelingmurphy@users.noreply.github.com>
2021-06-15 21:46:12 -0400

    create Results component to display zone result

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import Form from './components/Form'
+import Results from './components/Results'
 
 const App = () => {
     return (
       <div className="App">
         <Header />
         <Form />
+        <Results />
       </div>
     );
   }
---
+++ src/components/Results.js
@@ -0,0 +1,9 @@
+const Results = () => {
+    return (
+        <div>
+            <h2>Your Zone:</h2>
+        </div>
+    );
+}
+
+export default Results;
