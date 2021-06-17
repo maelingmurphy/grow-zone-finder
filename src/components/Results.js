74557fcc429f922b0b012d697dd3f08b78897681
Maeling <maelingmurphy@users.noreply.github.com>
2021-06-16 21:13:49 -0400

    add conditional logic that returns Results component only when zone and isCorrectFormat are true

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,14 @@
-const Results = ({ zone }) => {
-    return (
-        <div className='zone-container hidden'>
-            <h2 className='zone-title'>Your Zone: <span>{zone}</span></h2>
-        </div>
-    );
+const Results = ({ zone, isCorrectFormat }) => {
+    if (zone && isCorrectFormat) {
+        return (
+            <div className='zone-container'>
+                <h2 className='zone-title'>Your Zone: <span>{zone}</span></h2>
+            </div>
+        );
+    } else {
+        return null;
+    }
+   
 }
 
 export default Results;
