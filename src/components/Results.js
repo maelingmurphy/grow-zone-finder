aa2b6ab3e0866a2312efa5ef980f5cb667b36d59
Maeling <maelingmurphy@users.noreply.github.com>
2021-06-16 20:25:49 -0400

    hide Results component with hidden CSS class

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,6 @@
 const Results = ({ zone }) => {
     return (
-        <div className='zone-container'>
+        <div className='zone-container hidden'>
             <h2 className='zone-title'>Your Zone: <span>{zone}</span></h2>
         </div>
     );
