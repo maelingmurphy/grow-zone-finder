30e386b7b8d817ec15d39f89aa1b80bdd0727597
Maeling <maelingmurphy@users.noreply.github.com>
2021-06-18 18:20:49 -0400

    add form-items class to contain input field and button and set to display: flex, set width properties for form-items

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,16 +8,18 @@ const Form = ({ displayData, zipCode, onChange }) => {
             <form className='form'>
                 <div className='form-control'>
                     <label className='form-label' htmlFor='zipcode'>Enter your zip code to find your plant hardiness zone<span className="asterisk">*</span></label>
-                    <input
-                        className='form-input' 
-                        type='text'
-                        placeholder='Enter US zip code'
-                        name='zipcode'
-                        value={zipCode}
-                        onChange={onChange}
-                        autoComplete='off'
-                    />
-                    <Button text='Find My Zone' onClick={displayData}/>
+                    <div class="form-items">
+                        <input
+                            className='form-input' 
+                            type='text'
+                            placeholder='Enter US zip code'
+                            name='zipcode'
+                            value={zipCode}
+                            onChange={onChange}
+                            autoComplete='off'
+                        />
+                        <Button text='Find My Zone' onClick={displayData}/>
+                    </div>   
                     <p class="note"><span class="asterisk">*</span>Please note that information is not currently available for all US zip codes</p>
                 </div>
             </form>
