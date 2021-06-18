1f35c4a222c259a5a18abd8f3dfe5fe352d045a4
Maeling <maelingmurphy@users.noreply.github.com>
2021-06-18 14:52:10 -0400

    import leaf icon from font awesome

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
+import { FaLeaf } from 'react-icons/fa';
+
 const Header = () => {
     return(
         <div>
+            <FaLeaf />
             <h1>Find Your Grow Zone</h1>
         </div>
         
