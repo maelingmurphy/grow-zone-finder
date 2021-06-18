4689cf735d83a6a3e13078e5d3d6dd5dc20d0a46
Maeling <maelingmurphy@users.noreply.github.com>
2021-06-18 14:28:27 -0400

    change zone data display format and add classNames

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,7 +2,8 @@ const Results = ({ zone, isCorrectFormat }) => {
     if (zone && isCorrectFormat) {
         return (
             <div className='zone-container'>
-                <h2 className='zone-title'>Your Zone: <span>{zone}</span></h2>
+                <h2 className='zone-title'>Zone</h2>
+                <p className='zone-data'>{zone}</p>
             </div>
         );
     } else {
