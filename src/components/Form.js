d4935d732c0a869016a4fab7e52a7f2bb7fbcffc
Maeling <maelingmurphy@users.noreply.github.com>
2021-06-18 20:21:22 -0400

    add search icon and import IconContext for className styling, center note text

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,7 @@
 import Button from './Button';
 import './css/Form.css';
+import { IconContext } from 'react-icons'
+import { FaSearch } from 'react-icons/fa'
 
 const Form = ({ displayData, zipCode, onChange }) => {
   
@@ -9,15 +11,21 @@ const Form = ({ displayData, zipCode, onChange }) => {
                 <div className='form-control'>
                     <label className='form-label' htmlFor='zipcode'>Enter your zip code to find your plant hardiness zone<span className="asterisk">*</span></label>
                     <div class="form-items">
-                        <input
-                            className='form-input' 
-                            type='text'
-                            placeholder='Enter US zip code'
-                            name='zipcode'
-                            value={zipCode}
-                            onChange={onChange}
-                            autoComplete='off'
-                        />
+                        <div className='form-input-container'>
+                            <IconContext.Provider value={{className: 'icon-search'}}>
+                                <FaSearch size="1.4em" />
+                            </IconContext.Provider>
+                            <input
+                                className='form-input' 
+                                type='text'
+                                placeholder='Enter US zip code'
+                                name='zipcode'
+                                value={zipCode}
+                                onChange={onChange}
+                                autoComplete='off'
+                            />
+                        </div>
+                        
                         <Button text='Find My Zone' onClick={displayData}/>
                     </div>   
                     <p class="note"><span class="asterisk">*</span>Please note that information is not currently available for all US zip codes</p>
