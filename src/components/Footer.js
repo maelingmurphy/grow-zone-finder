b8b773ffab4f34dddd2f13fa3542f811f2a9a0d7
Maeling <maelingmurphy@users.noreply.github.com>
2021-06-19 16:30:55 -0400

    style footer link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './css/Footer.css';
 const Footer = () => {
     return (
         <footer>
-            <p>Made with <FaHeart color='#221500'/> by <span><a href="https://twitter.com/maelingcodes" target="_blank" rel="noreferrer">MaelingCodes</a></span> &copy; 2021</p>
+            <p>Made with <FaHeart color='#221500'/> by <span><a class="link" href="https://twitter.com/maelingcodes" target="_blank" rel="noreferrer">MaelingCodes</a></span> &copy; 2021</p>
         </footer>
     );
 }
