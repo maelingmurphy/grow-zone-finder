b25930a287f622e3ca38850de240fe5bce59b21c
Maeling <maelingmurphy@users.noreply.github.com>
2021-06-19 14:39:09 -0400

    create Footer.css and import to Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
-import { FaHeart } from 'react-icons/fa'
+import { FaHeart } from 'react-icons/fa';
+import './css/Footer.css';
 
 const Footer = () => {
     return (
-        <div>
+        <footer>
             <p>Made with <FaHeart color='#FBFFF8'/> by <span><a href="https://twitter.com/maelingcodes" target="_blank" rel="noreferrer">MaelingCodes</a></span> &copy; 2021</p>
-        </div>
+        </footer>
     );
 }
 
